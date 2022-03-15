ba4e9911401518b60f756b9df27dbd9a009bcc0a
Dzianis Herasimenka <Dzianis_Herasimenka@epam.com>
2022-03-15 15:45:27 +0300

    add all files for CostsProject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Costs from "./components/Costs/Costs";
+import NewCost from "./components/NewCost/NewCost";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const INITIAL_COSTS = [
+    {
+        id: 'c1',
+        date: new Date(2020, 2, 27),
+        description: "Холодильник",
+        amount: 999.99,
+    },
+    {
+        id: 'c2',
+        date: new Date(2021, 11, 12),
+        description: "MacBook",
+        amount: 1254.72,
+    },
+    {
+        id: 'c3',
+        date: new Date(2021, 4, 3),
+        description: "Jeans",
+        amount: 49.99,
+    }
+];
+
+const App = () => {
+
+    const [costs, setCosts] = useState(INITIAL_COSTS);
+
+    const addConstHandler = (cost) => {
+        setCosts((prevCosts => {
+            return [cost, ...prevCosts]
+        }));
+    }
+
+    return (
+        <div>
+            <NewCost onAddCost={addConstHandler} />
+            <Costs costs={costs} />
+
+
+        </div >
+    );
 }
 
 export default App;
---
+++ src/components/NewCost/NewCost.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './NewCost.css';
+import CostForm from './CostForm';
+
+const NewCost = (props) => {
+
+   const [isFormVisible, setIsFormVisible] = useState(false);
+
+   const saveCostDataHandler = (inputCostData) => {
+      const costData = {
+         ...inputCostData,
+         id: Math.random().toString()
+      };
+      props.onAddCost(costData);
+      setIsFormVisible(false);
+   };
+
+   const cancelCostHandler = () => {
+      setIsFormVisible(false)
+   }
+
+   const inputCostDataHandler = () => {
+      setIsFormVisible(true);
+   }
+
+   return (
+      <div className="new-cost">
+         {!isFormVisible && <button onClick={inputCostDataHandler}>Добавить новый расход</button>}
+         {isFormVisible && <CostForm onSaveCostData={saveCostDataHandler} onCancel={cancelCostHandler} />}
+      </div>
+   )
+}
+
+export default NewCost;
